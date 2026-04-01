fa03ab4b84f052cea7be1c3ac8109bbec85b38ee
victorbraz0980ty <victorbraz0980@gmail.com>
2026-04-01 10:05:22 -0300

    Teste pizzaria lopp.py

--- pizzaria lopp.py
+++ pizzaria lopp.py
@@ -21,23 +21,46 @@ customtkinter.set_default_color_theme("dark-blue")
 root = customtkinter.CTk()
 root.title("login dos usuários")
 root.geometry("500x300")
+
 def clique():
 
-    print("Seja bem - vindo a pizza loop, faça o seu login")
+    print("Seja bem - vindo a pizza loop, faça o seu login")    
+    
+'''Janela de cadastro e Posições'''  
+def abrir_cadastro():   
+    root.withdraw()    
+        
+    janela_de_cadastro = customtkinter.CTkToplevel(root)    
+
+    janela_de_cadastro.geometry("500x300")  
+
+    janela_de_cadastro.title("Cadastro")    
+
+    janela_de_cadastro.attributes("-topmost", True)     
+        
+    texto_cadastro = customtkinter.CTkLabel(janela_de_cadastro, text="Página de Cadastro")
+    texto_cadastro.pack(pady=20)
+
+
 texto = customtkinter.CTkLabel(root, text= "Seja bem - vindo a pizza loop, faça o seu login:", font=("Arial",14,"bold"))
 texto.pack(padx=10, pady=10)
 
 numero = customtkinter.CTkEntry(root, placeholder_text="Digite seu nome:")
 numero.pack(padx=10, pady=10)
-
+'''Senha e posiçao'''
 senha = customtkinter.CTkEntry(root, placeholder_text="Digite sua senha:", show="*")
 senha.pack(padx=10, pady=10)
-
+'''Lembrar login e posicao'''
 checkbox = customtkinter.CTkCheckBox(root, text="Lembrar login")
-checkbox.pack(padx=10, pady=10)
+checkbox.place(relx=0.15, rely=0.65)    
+'''Cadastro e posicao'''    
+cadrasto = customtkinter.CTkButton(root, text="Criar conta",command=abrir_cadastro,fg_color="transparent",text_color="white",bg_color="darkblue")
+cadrasto.place(relx=0.6, rely=0.65)
 
 botao= customtkinter.CTkButton(root, text="Login", command=clique)
 botao.pack(padx=10,pady=10)
+
+root.mainloop()
 
 root.mainloop()
 
